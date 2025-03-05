ab155c0a5bc1761e9f00f9d5a38924d81937d200
Janne <jp9435@gmail.com>
2025-03-05 19:21:05 +0200

    Added green nuuhkinta

--- jop_blobs.py
+++ jop_blobs.py
@@ -4,15 +4,40 @@
 import cv2
 import numpy as np
 
-def nothing():
+def nothing(x):
     pass
 
-cv2.namedWindow("Tracking")
+# Vihreän värin säätöpalikat
+cv2.namedWindow("Tracking_green")
+cv2.createTrackbar("low_hue_green", "Tracking_green", 0, 255, nothing)
+cv2.createTrackbar("low_satu_green", "Tracking_green", 0, 255, nothing)
+cv2.createTrackbar("low_value_green", "Tracking_green", 0, 255, nothing)
+cv2.createTrackbar("high_hue_green", "Tracking_green", 255, 255, nothing)
+cv2.createTrackbar("high_satu_green", "Tracking_green", 255, 255, nothing)
+cv2.createTrackbar("high_value_green", "Tracking_green", 255, 255, nothing)
 
 while True:
     frame = cv2.imread('blobs.png')
 
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Trackbaarit
+    l_h = cv2.getTrackbarPos("low_hue_green", "Tracking_green")
+    l_s = cv2.getTrackbarPos("low_satu_green", "Tracking_green")
+    l_v = cv2.getTrackbarPos("low_value_green", "Tracking_green")
+    u_h = cv2.getTrackbarPos("high_hue_green", "Tracking_green")
+    u_s = cv2.getTrackbarPos("high_satu_green", "Tracking_green")
+    u_v = cv2.getTrackbarPos("high_value_green", "Tracking_green")
+
+    l_b = np.array([l_h, l_s, l_v])
+    u_b = np.array([u_h, u_s, u_v])
+
+    mask = cv2.inRange(hsv, l_b, u_b)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
     cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
+    cv2.imshow("Result", res)
 
     key = cv2.waitKey(1)
     if key == 27:
